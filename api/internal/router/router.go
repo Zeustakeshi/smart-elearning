e7112bed45bea986d99a4b11eee857d306de5518
Minh Hiếu <gaconmvp2312@gmail.com>
2025-07-02 14:45:06 +0700

    Enables CORS for all origins
    Implements Cross-Origin Resource Sharing (CORS) middleware to allow requests from any origin.
    
    This configuration permits all common HTTP methods and headers,
    exposes the Content-Length header, allows credentials, and sets
    the maximum age for preflight requests to 12 hours.

--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -1,8 +1,10 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"smart-elearning/internal/middleware"
+	"time"
 )
 
 func NewRouter(
@@ -14,6 +16,14 @@ func NewRouter(
 	var router = gin.New()
 
 	/* GLOBAL MIDDLEWARE CONFIG */
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Use(middleware.ExceptionMiddleware)
 
 	/* AUTH */
