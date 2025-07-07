5445bf68cc1c5ce50cef68e0904a8a991d087a5d
Minh Hiếu <gaconmvp2312@gmail.com>
2025-07-07 09:45:53 +0700

    Refactor formatting in autoMigrate function for improved readability

--- api/internal/database/autoMigrate.go
+++ api/internal/database/autoMigrate.go
@@ -1,16 +1,17 @@
 package database
 
 import (
+	"smart-elearning/internal/entity"
 	"smart-elearning/pkg/log"
 )
 
 func AutoMigrate() {
 	err := DB.AutoMigrate(
-	&entity.User{},
-	&entity.Course{},
-	&entity.CourseMember{},
-	&entity.MultiChoiceLesson{},
-	&entity.MultiChoiceQuestion{},
+		&entity.User{},
+		&entity.Course{},
+		&entity.CourseMember{},
+		&entity.MultiChoiceLesson{},
+		&entity.MultiChoiceQuestion{},
 	)
 
 	if err != nil {
