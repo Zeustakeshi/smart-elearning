edec599eee959e22642523ef087c5e3b5cc1d4b4
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2025-07-11 15:08:18 +0700

    feat(teacher-v2): refactor detail lesson UI

--- ui/teacher-v2/src/routes/(app)/courses/$id/-components/CourseBanner.tsx
+++ ui/teacher-v2/src/routes/(app)/courses/$id/-components/CourseBanner.tsx
@@ -7,37 +7,37 @@ import CourseOverview from "./CourseOverview";
 type Props = {};
 
 const CourseBanner = ({}: Props) => {
-    return (
-        <div className=" relative w-full bg-stone-100 rounded-xl h-[220px] overflow-hidden">
-            {/*  BACKGROUND AND OVERLAY */}
-            <div className="z-[1] absolute top-0 left-0 w-full h-full select-none">
-                <img
-                    className="size-full object-cover"
-                    src="https://tse1.mm.bing.net/th/id/OIP.P1tCh-O3o_SWiGxh2NgxAgHaDQ?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
-                    alt=""
-                />
-            </div>
-            <div className="z-[2] absolute top-0 left-0 w-full h-full select-none bg-slate-900/70"></div>
+  return (
+    <div className=" relative w-full bg-stone-100 rounded-xl h-[220px] overflow-hidden">
+      {/*  BACKGROUND AND OVERLAY */}
+      <div className="z-[1] absolute top-0 left-0 w-full h-full select-none">
+        <img
+          className="size-full object-cover"
+          src="https://tse1.mm.bing.net/th/id/OIP.P1tCh-O3o_SWiGxh2NgxAgHaDQ?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+          alt=""
+        />
+      </div>
+      <div className="z-[2] absolute top-0 left-0 w-full h-full select-none bg-slate-900/70"></div>
 
-            {/*  */}
-            <div className="flex justify-between items-center w-full absolute top-0 left-0 z-10  p-5">
-                <div className="flex flex-col justify-start items-start">
-                    <div className=" w-full flex justify-start items-start gap-2 ">
-                        <CourseAvatar url="https://i.ibb.co/DfDmW1Cd/Gemini-Generated-Image-wdjbg2wdjbg2wdjb.png"></CourseAvatar>
-                        <div className="flex flex-col gap-1">
-                            <CourseName value="Giải tích 12"></CourseName>
-                            <CourseCode value="#123"></CourseCode>
-                            <CourseOverview></CourseOverview>
-                        </div>
-                    </div>
-                    <div className="mt-4 flex justify-start items-center gap-3 text-white">
-                        <Button variant="outline">Quản lý học sinh</Button>
-                        <Button variant="outline">Quản lý khóa học</Button>
-                    </div>
-                </div>
+      {/*  */}
+      <div className="flex justify-between items-center w-full absolute top-0 left-0 z-10  p-5">
+        <div className="flex flex-col justify-start items-start">
+          <div className=" w-full flex justify-start items-start gap-2 ">
+            <CourseAvatar url="https://i.pinimg.com/736x/2e/ae/fd/2eaefd75d164be0b17ef6f09749d0da8.jpg"></CourseAvatar>
+            <div className="flex flex-col gap-1">
+              <CourseName value="Giải tích 12"></CourseName>
+              <CourseCode value="#123"></CourseCode>
+              <CourseOverview></CourseOverview>
             </div>
+          </div>
+          <div className="mt-4 flex justify-start items-center gap-3 text-white">
+            <Button variant="outline">Quản lý học sinh</Button>
+            <Button variant="outline">Quản lý khóa học</Button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CourseBanner;
--- ui/teacher-v2/src/routes/(app)/courses/$id/route.tsx
+++ ui/teacher-v2/src/routes/(app)/courses/$id/route.tsx
@@ -5,56 +5,56 @@ import CourseBanner from "./-components/CourseBanner";
 import CourseDecription from "./-components/CourseDecription";
 
 export const Route = createFileRoute("/(app)/courses/$id")({
-    component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-    const { id: courseId } = Route.useParams();
-    console.log({ courseId });
-    return (
-        <div className="size-full">
-            <CourseBanner />
-            <CourseDecription
-                value={` Khóa học Toán 12 giúp học sinh hệ thống hóa kiến thức, rèn luyện kỹ năng giải bài tập và chuẩn bị tốt nhất cho kỳ thi tốt nghiệp THPT. Nội dung bao gồm các chuyên đề Đại số, Hình học, Tổ hợp - Xác suất, Giải tích, bám sát chương trình SGK và đề thi minh họa của Bộ Giáo dục. Khóa học cung cấp bài giảng chi tiết, ví dụ minh họa, bài tập tự luyện và hỗ trợ giải đáp thắc mắc, giúp học sinh tự tin đạt kết quả cao trong kỳ thi quan trọng này.`}
-            />
-            <div className="mt-5">
-                <Title className="mb-6">Nội dung khóa học</Title>
-                <div className="flex justify-start items-center gap-5 my-3">
-                    <Link
-                        activeProps={{
-                            className:
-                                "!border-b-primary text-primary transition-all font-semibold",
-                        }}
-                        activeOptions={{
-                            exact: true,
-                        }}
-                        className={cn(
-                            "text-lg border-b-3  border-b-transparent hover:text-primary transition-all hover:font-semibold"
-                        )}
-                        to="/courses/$id/lessons"
-                        params={{ id: courseId }}
-                    >
-                        Bài học
-                    </Link>
-                    <Link
-                        activeProps={{
-                            className:
-                                "!border-b-primary text-primary transition-all font-semibold",
-                        }}
-                        activeOptions={{
-                            exact: true,
-                        }}
-                        className={cn(
-                            "text-lg border-b-3 border-b-transparent hover:text-primary transition-all hover:font-semibold"
-                        )}
-                        to="/courses/$id/documents"
-                        params={{ id: courseId }}
-                    >
-                        Tài liệu
-                    </Link>
-                </div>
-                <Outlet></Outlet>
-            </div>
+  const { id: courseId } = Route.useParams();
+  console.log({ courseId });
+  return (
+    <div className="size-full flex flex-col space-y-6">
+      <CourseBanner />
+      <CourseDecription
+        value={` Khóa học Toán 12 giúp học sinh hệ thống hóa kiến thức, rèn luyện kỹ năng giải bài tập và chuẩn bị tốt nhất cho kỳ thi tốt nghiệp THPT. Nội dung bao gồm các chuyên đề Đại số, Hình học, Tổ hợp - Xác suất, Giải tích, bám sát chương trình SGK và đề thi minh họa của Bộ Giáo dục. Khóa học cung cấp bài giảng chi tiết, ví dụ minh họa, bài tập tự luyện và hỗ trợ giải đáp thắc mắc, giúp học sinh tự tin đạt kết quả cao trong kỳ thi quan trọng này.`}
+      />
+      <div className="mt-5">
+        <Title className="mb-6">Nội dung khóa học</Title>
+        <div className="flex justify-start items-center gap-5 my-3">
+          <Link
+            activeProps={{
+              className:
+                "!border-b-primary text-primary transition-all font-semibold",
+            }}
+            activeOptions={{
+              exact: true,
+            }}
+            className={cn(
+              "text-lg border-b-3  border-b-transparent hover:text-primary transition-all hover:font-semibold"
+            )}
+            to="/courses/$id/lessons"
+            params={{ id: courseId }}
+          >
+            Bài học
+          </Link>
+          <Link
+            activeProps={{
+              className:
+                "!border-b-primary text-primary transition-all font-semibold",
+            }}
+            activeOptions={{
+              exact: true,
+            }}
+            className={cn(
+              "text-lg border-b-3 border-b-transparent hover:text-primary transition-all hover:font-semibold"
+            )}
+            to="/courses/$id/documents"
+            params={{ id: courseId }}
+          >
+            Tài liệu
+          </Link>
         </div>
-    );
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 }
