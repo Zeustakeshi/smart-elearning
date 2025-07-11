133b1a0c2398a6cb156924bc7a65aff23451beee
Vohoangtuanhdhtdm <2224801030161@student.tdmu.edu.vn>
2025-07-11 13:05:56 +0700

    Refactor login and route components for improved readability and structure

--- ui/teacher-v2/src/routes/auth/route.tsx
+++ ui/teacher-v2/src/routes/auth/route.tsx
@@ -1,25 +1,23 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { Toaster } from "react-hot-toast";
 export const Route = createFileRoute("/auth")({
-    component: RouteComponent,
-    beforeLoad({ context }) {
-        if (context.auth.isAuthenticated) {
-            throw redirect({
-                to: "/",
-            });
-        }
-    },
+  component: RouteComponent,
+  beforeLoad({ context }) {
+    if (context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/",
+      });
+    }
+  },
 });
 
 function RouteComponent() {
-    return (
-        <div className="grid grid-cols-10 w-screen h-screen">
-            <div className="absolute top-0 z-[-2] h-screen w-screen bg-stone-100 bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
-            <div className="col-span-5 h-full bg-blue-500"></div>
-            <div className="col-span-5 h-full flex justify-center items-center">
-                <Outlet></Outlet>
-                <Toaster position="top-right" reverseOrder={true} />
-            </div>
-        </div>
-    );
+  return (
+    <div className=" w-screen h-screen flex justify-center items-center bg-gradient-to-br from-sky-300 via-blue-200 to-indigo-300">
+      <div className=" w-2xl flex justify-center items-center shadow-lg bg-white  rounded-3xl">
+        <Outlet></Outlet>
+        <Toaster position="top-right" reverseOrder={true} />
+      </div>
+    </div>
+  );
 }
